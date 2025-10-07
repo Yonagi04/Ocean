c3803c0756d3af3ee50d03ba323c30cbe295571f
Yonagi <1809266700@qq.com>
2025-10-07 16:26:15 +0800

    Feat: HTTP支持HEAD方法

--- src/main/java/com/yonagi/ocean/handler/impl/HeadHandler.java
+++ src/main/java/com/yonagi/ocean/handler/impl/HeadHandler.java
@@ -1,8 +1,14 @@
 package com.yonagi.ocean.handler.impl;
 
 import com.yonagi.ocean.core.protocol.HttpRequest;
+import com.yonagi.ocean.core.protocol.HttpResponse;
+import com.yonagi.ocean.core.protocol.HttpVersion;
 import com.yonagi.ocean.handler.RequestHandler;
+import com.yonagi.ocean.utils.MimeTypeUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -15,8 +21,55 @@ import java.io.OutputStream;
  */
 public class HeadHandler implements RequestHandler {
 
+    private final String webRoot;
+
+    private static final Logger log = LoggerFactory.getLogger(HeadHandler.class);
+
+    public HeadHandler(String webRoot) {
+        this.webRoot = webRoot;
+    }
+
     @Override
     public void handle(HttpRequest request, OutputStream output) throws IOException {
+        String uri = request.getUri();
+        if ("/".equals(uri)) {
+            uri = "index.html";
+        }
+        if (uri.startsWith(webRoot)) {
+            uri = uri.substring(webRoot.length());
+        }
+        File file = new File(webRoot, uri);
+        if (!file.exists() || file.isDirectory()) {
+            writeNotFound(output);
+            return;
+        }
+        if (!file.getCanonicalPath().startsWith(new File(webRoot).getCanonicalPath())) {
+            writeNotFound(output);
+            log.warn("Attempted directory traversal attack: {}", uri);
+            return;
+        }
+        String contentType = MimeTypeUtil.getMimeType(file.getName());
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        HttpResponse response = new HttpResponse.Builder()
+                .httpVersion(request.getHttpVersion())
+                .statusCode(200)
+                .statusText("OK")
+                .contentType(contentType)
+                .build();
+        output.write(response.toString().getBytes());
+        output.flush();
+    }
 
+    private void writeNotFound(OutputStream output) throws IOException {
+        HttpResponse response = new HttpResponse.Builder()
+                .httpVersion(HttpVersion.HTTP_1_1)
+                .statusCode(404)
+                .statusText("Not Found")
+                .contentType("text/html")
+                .build();
+        output.write(response.toString().getBytes());
+        output.flush();
     }
 }
